eb6b1ceae9805c6b69e95b82bee273941108e407
juliaprzygoda <przygodajulia2@gmail.com>
2024-03-23 09:05:34 +0100

    Adding test view

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hi there 👋';
+require_once 'src/controllers/AppController.php';
+
+$controller = new AppController();
+
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+$action = explode("/", $path)[0];
+
+$controller->render($action);
